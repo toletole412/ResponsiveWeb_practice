5fb82886de007f53d6c913797c63b48706b700e7
toletole412 <yoonjioh90@gmail.com>
2018-05-10 16:34:42 +0200

    add move

---
+++ src/components/Animation.js
@@ -0,0 +1,21 @@
+import React, {PureComponent} from 'react';
+import './Animation.css';
+
+export default class Animation extends PureComponent {
+  render() {
+    return (
+    <div>
+      <div className="changeColor">
+      </div>
+      <div className="fourColors">
+      </div>
+      <div className="move">
+        <div className="move" id="div1"></div>
+        <div className="move" id="div2"></div>
+        <div className="move" id="div3"></div>
+        <div className="move" id="div4"></div>
+      </div>
+    </div>
+    )
+  }
+}
