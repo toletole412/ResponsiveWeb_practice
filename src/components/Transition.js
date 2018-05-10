f673a4424e1d6cba94e50ca8814cc49f7deea260
toletole412 <yoonjioh90@gmail.com>
2018-05-10 16:05:47 +0200

    add Transition component

---
+++ src/components/Transition.js
@@ -0,0 +1,19 @@
+import React, {PureComponent} from 'react';
+import './Transition.css';
+
+export default class Transition extends PureComponent {
+  render() {
+    return (
+    <div>
+      <div className="red">
+      </div>
+      <div className="dodgerblue">
+      </div>
+      <div className="grey">
+      </div>
+      <div className="tomato">
+      </div>
+    </div>
+    )
+  }
+}
