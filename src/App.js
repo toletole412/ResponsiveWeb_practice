2af36038652997a48f275498d42b86d0da291214
toletole412 <yoonjioh90@gmail.com>
2018-05-10 16:04:58 +0200

    render transition component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import Article from './components/Article';
+import Transition from './components/Transition';
 
 class App extends Component {
   render() {
     return (
+    <div>
       <Article />
+      <Transition />
+    </div>
     );
   }
 }
